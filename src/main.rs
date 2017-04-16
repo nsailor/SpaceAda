c53cf7da07c8bc77e4e2bb06b69b3c69faf722f8
Jason P. Barmparesos <jason.barmparesos@gmail.com>
2017-04-16 10:31:08 +0300

    Parse prototypes.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod data_type;
 mod expression;
 mod statement;
+mod prototype;
 mod ada_grammar {
     include!(concat!(env!("OUT_DIR"), "/ada_grammar.rs"));
 }
@@ -9,5 +10,7 @@ mod ada_grammar {
 fn main() {
     println!("SpaceAda Compiler v0.0.1 Pre-Alpha");
     println!("Not much to do, bye!");
-    println!("{:?}", ada_grammar::statement("Fire(64, 55);"));
+    println!("{:?}",
+             ada_grammar::prototype("procedure Fire_Missile(Target_X : in Integer; Target_Y : \
+                                     in Integer)"));
 }
---
+++ src/prototype.rs
@@ -0,0 +1,24 @@
+
+use data_type::DataType;
+use data_type;
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Prototype {
+    pub name: String,
+    pub returns: Option<data_type::DataType>,
+    pub arguments: Vec<ParameterDeclaration>,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ParameterDirection {
+    In,
+    Out,
+    InOut,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct ParameterDeclaration {
+    pub data_type: data_type::DataType,
+    pub name: String,
+    pub direction: ParameterDirection,
+}
